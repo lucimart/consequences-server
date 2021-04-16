a3e6cb1abdd8c3f12cb21833c722877bf7805155
Lucía Martínez <luci.dmartinezr@gmail.com>
2021-04-16 22:00:08 +0200

    update resolvers

--- src/resolvers/Post.ts
+++ src/resolvers/Post.ts
@@ -1,28 +1,28 @@
 // @ts-ignore
 const author = async (parent, _, context) => {
   return await context.prisma.author
-    .findUnique({ where: { id: parent.id } })
+    .findUnique({ where: { id: parent.authorId } })
     .author();
 };
 
 // @ts-ignore
 const group = async (parent, _, context) => {
   return await context.prisma.group
-    .findUnique({ where: { id: parent.id } })
+    .findUnique({ where: { id: parent.groupId } })
     .group();
 };
 
 // @ts-ignore
 const text = async (parent, _, context) => {
   return await context.prisma.text
-    .findUnique({ where: { id: parent.id } })
+    .findUnique({ where: { postId: parent.id } })
     .text();
 };
 
 // @ts-ignore
 const image = async (parent, _, context) => {
   return await context.prisma.image
-    .findUnique({ where: { id: parent.id } })
+    .findUnique({ where: { postId: parent.id } })
     .image();
 };
 
